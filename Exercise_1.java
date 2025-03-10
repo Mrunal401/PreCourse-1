d81ef0efe02cf3c15578f150114bea6fc7ff4bf8
Mrunal401 <52656959+Mrunal401@users.noreply.github.com>
2025-03-09 23:09:33 -0700

    completed Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,5 @@
+//Time complexity = O(1)
+//Space Complexity = O(n)
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -8,28 +10,56 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+        if (top == -1)
+            return true;
+        else
+            return false;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if(top >= (MAX-1)){
+            System.err.println("Stack Overflow");
+            return false;
+        }
+        else {
+            a[++top] = x;
+            System.err.println("Pushed: "+a[top]);
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+        if(isEmpty()){
+            System.err.println("Stack Underflow");
+            return 0;
+        }
+        else{
+            return a[top--];
+        }
     } 
   
     int peek() 
     { 
         //Write your code here
+        if(isEmpty()){
+            System.err.println("Stack is empty");
+            return false;
+        }
+        else{
+            return a[top];
+        }
     } 
 } 
   
